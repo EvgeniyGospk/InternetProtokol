af32ecb28625b621684b8480e80a5405973c4b50
EvgeniyGospk <evgeniy.gosp.univer@gmail.com>
2022-12-18 10:06:00 +0300

    Добавлены начальные данные

--- main-project/main.cpp
+++ main-project/main.cpp
@@ -6,8 +6,11 @@ using namespace std;
 int main()
 {
 	setlocale(LC_ALL, "ru");
+
+
+
         cout<<"Вариант 5, Протокол работы в Интернет, Господарик Евгений"<<endl;
 
-
-	return 0;
+		cout << "Почему не выводи" << endl;
+		return 0;
 }
